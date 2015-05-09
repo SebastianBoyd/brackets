d13da5b51f2c2e30e641b2e16c5204396645e417
Sebastian Boyd <lord.of.all.sebastian@gmail.com>
2015-05-09 13:37:11 -0700

    Remove stuff

--- src/filesystem/impls/demo/AjaxFileSystem.js
+++ src/filesystem/impls/demo/AjaxFileSystem.js
@@ -1,24 +1,24 @@
 /*
  * Copyright (c) 2014 Adobe Systems Incorporated. All rights reserved.
- *  
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"), 
- * to deal in the Software without restriction, including without limitation 
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- * and/or sell copies of the Software, and to permit persons to whom the 
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- *  
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *  
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
- * 
+ *
  */
 
 
@@ -31,11 +31,11 @@
  */
 define(function (require, exports, module) {
     "use strict";
-    
+
     var FileSystemError = require("filesystem/FileSystemError"),
         FileSystemStats = require("filesystem/FileSystemStats");
-    
-    
+
+
     /**
      * To avoid spamming console with 404s, we hardcode a few known non-existent files
      */
@@ -44,11 +44,11 @@ define(function (require, exports, module) {
 //            console.log("Assuming " + path + " does not exist");
             return false;
         }
-        
+
         return true;  // TODO: use $.get(HEAD) to check if it really exists?
     }
-    
-    
+
+
     function stat(path, callback) {
         if (!_exists(path)) {
             callback(FileSystemError.NOT_FOUND);
@@ -62,7 +62,7 @@ define(function (require, exports, module) {
             callback(null, stats);
         }
     }
-    
+
     function readFile(path, callback) {
         if (!_exists(path)) {
             callback(FileSystemError.NOT_FOUND);
@@ -79,7 +79,7 @@ define(function (require, exports, module) {
             });
         }
     }
-    
+
     // Export public API
     exports.stat        = stat;
     exports.readFile    = readFile;
