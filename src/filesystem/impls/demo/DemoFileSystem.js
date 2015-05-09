d774a886eff69cd94eef4d51ca31f09f7baa463a
Sebastian Boyd <lord.of.all.sebastian@gmail.com>
2015-05-08 22:14:55 -0700

    White space

--- src/filesystem/impls/demo/DemoFileSystem.js
+++ src/filesystem/impls/demo/DemoFileSystem.js
@@ -1,24 +1,24 @@
 /*
  * Copyright (c) 2013 Adobe Systems Incorporated. All rights reserved.
- *  
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"), 
- * to deal in the Software without restriction, including without limitation 
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- * and/or sell copies of the Software, and to permit persons to whom the 
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- *  
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *  
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
- * 
+ *
  */
 
 
@@ -27,20 +27,20 @@
 
 define(function (require, exports, module) {
     "use strict";
-    
+
     var FileSystemError = require("filesystem/FileSystemError"),
         FileSystemStats = require("filesystem/FileSystemStats"),
         AjaxFileSystem  = require("filesystem/impls/demo/AjaxFileSystem");
-    
-    
+
+
     // Brackets uses FileSystem to read from various internal paths that are not in the user's project storage. We
     // redirect core-extension access to a simple $.ajax() to read from the source code location we're running from,
     // and for now we ignore we possibility of user-installable extensions or persistent user preferences.
     var CORE_EXTENSIONS_PREFIX = PathUtils.directory(window.location.href) + "extensions/default/";
 //    var USER_EXTENSIONS_PREFIX = "/.brackets.user.extensions$/";
 //    var CONFIG_PREFIX = "/.$brackets.config$/";
-    
-    
+
+
     // Static, hardcoded file tree structure to serve up. Key is entry name, and value is either:
     //  - string = file
     //  - object = nested folder containing more entries
@@ -49,16 +49,16 @@ define(function (require, exports, module) {
         "main.css": ".hello {\n    content: 'world!';\n}",
         "main.js": "function sayHello() {\n    console.log('Hello, world!');\n}"
     };
-    
-    
+
+
     function _startsWith(path, prefix) {
         return (path.substr(0, prefix.length) === prefix);
     }
-    
+
     function _stripTrailingSlash(path) {
         return path[path.length - 1] === "/" ? path.substr(0, path.length - 1) : path;
     }
-    
+
     function _getDemoData(fullPath) {
         var prefix = "/Getting Started/";
         if (fullPath.substr(0, prefix.length) !== prefix) {
@@ -68,7 +68,7 @@ define(function (require, exports, module) {
         if (!suffix) {
             return demoContent;
         }
-        
+
         var segments = suffix.split("/");
         var dir = demoContent;
         var i;
@@ -78,7 +78,7 @@ define(function (require, exports, module) {
         }
         return dir;
     }
-    
+
     function _makeStat(demoData) {
         var options = {
             isFile: typeof demoData === "string",
@@ -94,14 +94,14 @@ define(function (require, exports, module) {
         var segments = _stripTrailingSlash(path).split("/");
         return segments[segments.length - 1];
     }
-    
-    
+
+
     function stat(path, callback) {
         if (_startsWith(path, CORE_EXTENSIONS_PREFIX)) {
             AjaxFileSystem.stat(path, callback);
             return;
         }
-        
+
         var result = _getDemoData(path);
         if (result || result === "") {
             callback(null, _makeStat(result));
@@ -109,7 +109,7 @@ define(function (require, exports, module) {
             callback(FileSystemError.NOT_FOUND);
         }
     }
-    
+
     function exists(path, callback) {
         stat(path, function (err) {
             if (err) {
@@ -119,13 +119,13 @@ define(function (require, exports, module) {
             }
         });
     }
-    
+
     function readdir(path, callback) {
         if (_startsWith(path, CORE_EXTENSIONS_PREFIX)) {
             callback("Directory listing unavailable: " + path);
             return;
         }
-        
+
         var storeData = _getDemoData(path);
         if (!storeData) {
             callback(FileSystemError.NOT_FOUND);
@@ -140,18 +140,18 @@ define(function (require, exports, module) {
             callback(null, names, stats);
         }
     }
-    
+
     function mkdir(path, mode, callback) {
         callback("Cannot modify folders on HTTP demo server");
     }
-    
+
     function rename(oldPath, newPath, callback) {
         callback("Cannot modify files on HTTP demo server");
     }
-    
+
     function readFile(path, options, callback) {
         console.log("Reading 'file': " + path);
-        
+
         if (typeof options === "function") {
             callback = options;
         }
@@ -160,7 +160,7 @@ define(function (require, exports, module) {
             AjaxFileSystem.readFile(path, callback);
             return;
         }
-        
+
         var storeData = _getDemoData(path);
         if (!storeData && storeData !== "") {
             callback(FileSystemError.NOT_FOUND);
@@ -171,48 +171,48 @@ define(function (require, exports, module) {
             callback(null, storeData, _makeStat(storeData[name]));
         }
     }
-    
-    
+
+
     function writeFile(path, data, options, callback) {
         callback("Cannot save to HTTP demo server");
     }
-    
+
     function unlink(path, callback) {
         callback("Cannot modify files on HTTP demo server");
     }
-    
+
     function moveToTrash(path, callback) {
         callback("Cannot delete files on HTTP demo server");
     }
-    
+
     function initWatchers(changeCallback, offlineCallback) {
         // Ignore - since this FS is immutable, we're never going to call these
     }
-    
+
     function watchPath(path, callback) {
         console.warn("File watching is not supported on immutable HTTP demo server");
         callback();
     }
-    
+
     function unwatchPath(path, callback) {
         callback();
     }
-    
+
     function unwatchAll(callback) {
         callback();
     }
-    
+
     function showOpenDialog(allowMultipleSelection, chooseDirectories, title, initialPath, fileTypes, callback) {
         // FIXME
         throw new Error();
     }
-    
+
     function showSaveDialog(title, initialPath, proposedNewFilename, callback) {
         // FIXME
         throw new Error();
     }
 
-    
+
     // Export public API
     exports.showOpenDialog  = showOpenDialog;
     exports.showSaveDialog  = showSaveDialog;
@@ -229,7 +229,7 @@ define(function (require, exports, module) {
     exports.watchPath       = watchPath;
     exports.unwatchPath     = unwatchPath;
     exports.unwatchAll      = unwatchAll;
-    
+
     exports.recursiveWatch    = true;
     exports.normalizeUNCPaths = false;
 });
